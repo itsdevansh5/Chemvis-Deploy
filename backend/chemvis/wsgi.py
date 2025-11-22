015fd0b9da3f6f998e43b206982d782b6cf3261a
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-11-23 00:18:12 +0530

    fix wsgi file

--- backend/chemvis/wsgi.py
+++ backend/chemvis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chemvis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.chemvis.settings')
 
 application = get_wsgi_application()
