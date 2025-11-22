116683dacf2ef94c5b066f5337fd983465659118
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-11-22 23:46:44 +0530

    configured backend for deployment

--- backend/chemvis/settings.py
+++ backend/chemvis/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +29,7 @@ SECRET_KEY = 'django-insecure-7j98@!kausn%4z)zq_u687gr*yr-)dcqb@iqde11^dbqkcrj=t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
